75864d8095e45495aae487e8442e9b048a1c1ce2
俊 朱 <781894956@qq.com>
2019-05-24 20:44:24 +0800

    创建公司MODEL

--- HYSYS/DataContext.cs
+++ HYSYS/DataContext.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using HYSYS.Models;
 using WalkingTec.Mvvm.Core;
 
 namespace HYSYS
 {
     public class DataContext : FrameworkContext
     {
+        public DbSet<Company> Companies { get; set; }
         public DataContext(string cs, DBTypeEnum dbtype)
              : base(cs, dbtype)
         {
---
+++ HYSYS/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WalkingTec.Mvvm.Core;
+
+namespace HYSYS.Models
+{
+    public class Company:BasePoco
+    {
+        [Display(Name = "公司编码")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [RegularExpression("^[0-9]{3,3}$", ErrorMessage = "{0}必须是3位数字")]
+        [StringLength(3)]
+        public string CompanyCode { get; set; }
+        [Display(Name = "公司名称")]
+        [StringLength(50, ErrorMessage = "{0}最多输入{1}个字符")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "备注")]
+        public string Remark { get; set; }
+    }
+}
